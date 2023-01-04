d0d36ee6cc62dbe63f19ed8f2d1f6c3ddfd3a1a3
markian mwangi <50865281+markmumba@users.noreply.github.com>
2023-01-04 19:42:41 +0300

    finished the static webpage

---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+export default function Header () {
+    return (
+        <div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+        <a class="navbar-brand" href="#root">
+            <img src="./logo512.png" alt="" width="30" height="24" class="d-inline-block align-text-top"></img>
+            ReactFacts
+        </a>
+        <div className="project1">
+            <h4>  Course-Project 1</h4>
+        </div>
+        </div>
+    </nav>
+        </div>
+    )
+}
---
+++ src/components/maincontent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default function MainContent () {
+    return (
+        <div>
+            <h1 > Fun facts about React</h1>
+            <div className="list-items">
+            <ul className="lists">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintaind by facebook </li>
+                <li>Powers thousands of enterprise apps,including mobile apps</li>
+            </ul>
+            </div>
+        </div>
+    )
+}
